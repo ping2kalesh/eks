c19ad3b61c762b3e51004c9df5c0002eaa91d33b
Kalesha Shaik <ping2kalesh@gmail.com>
2022-01-16 01:40:31 +0530

    updates autoassign public ip

--- network.tf
+++ network.tf
@@ -11,9 +11,10 @@ resource "aws_vpc" "eks-vpc" {
 }
 
 resource "aws_subnet" "eks-pubsubnet01" {
-  vpc_id            = aws_vpc.eks-vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  vpc_id                  = aws_vpc.eks-vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name                                        = "eks-pubsubnet01"
@@ -22,9 +23,10 @@ resource "aws_subnet" "eks-pubsubnet01" {
 }
 
 resource "aws_subnet" "eks-pubsubnet02" {
-  vpc_id            = aws_vpc.eks-vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
+  vpc_id                  = aws_vpc.eks-vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
 
   tags = {
     Name                                        = "eks-pubsubnet02"
@@ -34,9 +36,10 @@ resource "aws_subnet" "eks-pubsubnet02" {
 
 
 resource "aws_subnet" "eks-pubsubnet03" {
-  vpc_id            = aws_vpc.eks-vpc.id
-  cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-east-1c"
+  vpc_id                  = aws_vpc.eks-vpc.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-east-1c"
+  map_public_ip_on_launch = true
 
   tags = {
     Name                                        = "eks-pubsubnet03"
