7203d10684d9952266c6fd35cb0021b1c9c22710
Kalesha Shaik <ping2kalesh@gmail.com>
2022-01-16 11:53:10 +0530

    updates the version to 1.20

--- eks.tf
+++ eks.tf
@@ -1,7 +1,7 @@
 resource "aws_eks_cluster" "kalesha-eks-cluster" {
   name     = "kalesha-eks-cluster"
   role_arn = "arn:aws:iam::473745473587:role/myAmazonEKSClusterRole"
-  version  = "1.18"
+  version  = "1.20"
   vpc_config {
     subnet_ids = [aws_subnet.eks-pubsubnet01.id, aws_subnet.eks-pubsubnet02.id, aws_subnet.eks-pubsubnet03.id]
   }
